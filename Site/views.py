01f1f95fddf6b186d4378b7250901d59754a455a
Saif Abdullah Mahi <samahicse@gmail.com>
2024-05-19 20:40:25 +0600

    User Name and Avater updated according to the authenticated user profile

--- Site/views.py
+++ Site/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import  login_required 
+from Users.models import Person
 
 # Create your views here.
 
 
 @login_required
 def HomepageLoggedInView(request) :
-    return render(request ,"Site\HomepageLoggedIn.html")
+    user = Person.objects.get(user_name = request.session['user_name'])
+    return render(request ,"Site\HomepageLoggedIn.html" , {
+        "user" : user.first_name +' '+ user.last_name,
+        "gender": user.gender
+    })
 
 
 def custom_logout(request):
