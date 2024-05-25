dfc564776043b2df478b17ce36444cd8a9f6e725
Saif <samahicse@gmail.com>
2024-05-26 01:28:44 +0600

    Cover Photo has been fixed for the User Profile Page

--- Site/urls.py
+++ Site/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
 
     path("" , views.HomepageLoggedInView , name ="homepageLoggedIn"),
     path("logout/",views.custom_logout , name='custom_logout' ),
-    path("profile/",views.ProfileView , name='profile' )
+    path("profile/",views.ProfileView , name='profile' ),
+    path('update_cover_photo/', views.update_cover_photo, name='update_cover_photo'),
+    path('delete_cover_photo/', views.delete_cover_photo, name='delete_cover_photo'),
         
 ]
 
--- Users/models.py
+++ Users/models.py
@@ -1,18 +1,40 @@
 from django.db import models
+import os
 
-# Create your models here.
+def user_directory_path(instance, filename):
+    # File will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return f'{instance.user_name}/{filename}'
+
 class Person(models.Model):
-    user_name = models.CharField(max_length=125, unique=True ,primary_key=True)
+    user_name = models.CharField(max_length=125, unique=True, primary_key=True)
     first_name = models.CharField(max_length=125)
     last_name = models.CharField(max_length=125)
     email = models.EmailField(null=True)
-    dob = models.DateField(null=True , blank=True)
+    dob = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=10)
     password1 = models.CharField(max_length=20)
-    password2= models.CharField(max_length=20)
-    
+    password2 = models.CharField(max_length=20)
+    cover_photo = models.ImageField(upload_to=user_directory_path, null=True)
+    profile_photo = models.ImageField(upload_to=user_directory_path, null=True)
 
     def __str__(self):
-        return self.user_name+": " + self.first_name +" "+self.last_name 
-    
-     
+        return f'{self.user_name}: {self.first_name} {self.last_name}'
+
+    def save(self, *args, **kwargs):
+        # Check if the user has uploaded the first cover photo
+        if self.cover_photo and not hasattr(self, '_cover_photo'):
+            # Create directory if it doesn't exist
+            directory = os.path.join('media', user_directory_path(self, ''))
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            self._cover_photo = True  # Set a flag to indicate that cover photo is uploaded
+
+        # Check if the user has uploaded the first profile photo
+        if self.profile_photo and not hasattr(self, '_profile_photo'):
+            # Create directory if it doesn't exist
+            directory = os.path.join('media', user_directory_path(self, ''))
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            self._profile_photo = True  # Set a flag to indicate that profile photo is uploaded
+
+        super().save(*args, **kwargs)
